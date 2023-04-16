3570af4e4628dd72fef0b72be18575b0f806fc58
Marcos Fábio <mfabiosilva49@gmail.com>
2023-04-16 00:57:14 -0300

    Implementação integral do sistema

--- Main/Main.ino
+++ Main/Main.ino
@@ -1,3 +1,5 @@
+#include <ESP8266WiFi.h>     // includes the ESP8266WiFi library for configuring the Wi-Fi connection
+#include <PubSubClient.h>    // includes the PubSubClient library for connecting to an MQTT server
 #include <IRrecv.h>
 #include <IRsend.h>
 #include <Arduino.h>
@@ -6,6 +8,14 @@
 #include <ir_Daikin.h>
 #include <ir_Fujitsu.h>
 #include <ir_Toshiba.h>
+
+const char* nameWifi = "Marcos_NET";                // Wi-Fi network name
+const char* password = "05102202";                 // Wi-Fi network password
+
+const char* server_MQTT = "broker.hivemq.com"; // MQTT server address
+
+WiFiClient espClient;                           // Creates a WiFiClient object
+PubSubClient client(espClient);                 // Creates a PubSubClient object and passes the WiFiClient object as parameter
 
 // Hardware and time settings
 const uint16_t kRecvPin = 14;             // D5; pin to which the infrared receiver is connected.
@@ -89,18 +99,7 @@ void decodeToshiba(decode_results *decodedSignal)
 #endif // DECODE_TOSHIBA_AC
 }
 
-void setup()
-{
-  Serial.begin(kBaudRate);
-#if DECODE_HASH
-  irrecv.setUnknownThreshold(kMinUnknownSize); // Ignore messages with less than the minimum on or off pulses.
-#endif                                         // DECODE_HASH
-  irrecv.enableIRIn();                         // Starts IR signal reception
-  irsend.begin();                              // Start IR signal emitter
-}
-
-void loop()
-{
+void receiveDecodeSignals(){
   decode_results currentDecodedSignal;
 
   if (irrecv.decode(&currentDecodedSignal))
@@ -122,9 +121,7 @@ void loop()
     Serial.println(resultToSourceCode(&currentDecodedSignal));
     yield();
 
-    if (decodingCounter < MAX_DECODING_COUNTER)
-    {
-      if (isTurnedOn)
+    if (isTurnedOn)
       {
         Serial.println("Sinal de desligar decodificado!");
         turnOffSignal.rawData = resultToRawArray(&currentDecodedSignal);
@@ -138,32 +135,115 @@ void loop()
       }
 
       isTurnedOn = !isTurnedOn; // inverte o valor da variável
-      decodingCounter++;
-    }
-    else
-    {
-      Serial.println("Recebi um sinal, mas não vou decodificar porque excedi o número máximo de decodificações permitidas.");
-    }
-    irrecv.resume();
+   irrecv.resume();
+  } 
+}
+
+void setup() {
+  Serial.begin(115200);                         // Initializes serial communication at the speed of 115200 bps
+  connect_WiFi();                              // Calls the connect_WiFi() function to connect to the Wi-Fi network
+  client.setServer(server_MQTT, 1883);         // Configures the MQTT server with the address and port
+  client.setCallback(callback);                 // Set the callback function to receive MQTT messages
+
+  Serial.begin(kBaudRate);
+  #if DECODE_HASH
+    irrecv.setUnknownThreshold(kMinUnknownSize); // Ignore messages with less than the minimum on or off pulses.
+  #endif                                         // DECODE_HASH
+  irrecv.enableIRIn();                         // Starts IR signal reception
+  irsend.begin();                              // Start IR signal emitter
+
+  while (decodingCounter < MAX_DECODING_COUNTER){
+    Serial.println("Sistema pronto para receber o sinal IR!");
+    receiveDecodeSignals(); 
+    decodingCounter++;
   }
 
-  if (Serial.available() > 0)
-  {
-    irrecv.disableIRIn();
-    int readCommand = Serial.read();
+  irrecv.disableIRIn();
+}
 
-    if (readCommand == TURN_ON_COMMAND)
-    {
-      irsend.sendRaw(turnOnSignal.rawData, turnOnSignal.rawDataLength, 38);
-      Serial.println("Ligando o ar-condicionado...");
-      //Serial.println(resultToSourceCode(&turnOnSignal));
+void loop() { 
+  if(!client.connected()){                      // Checks if the MQTT client is connected
+    reconnect();                                 // If not connected, calls the reconnect() function to connect again
+  }
+  client.loop();                                 // Loops the MQTT client
+}
+
+void connect_WiFi(){
+  delay(10);                                     // Wait a short period of time
+  Serial.println();
+  Serial.print("Conectando na rede: ");
+  Serial.println(nameWifi);
+  WiFi.mode(WIFI_STA);                           // Configures the ESP8266 module as a Wi-Fi client
+  WiFi.begin(nameWifi, password);                       // Connects to the specified Wi-Fi network with the provided name and password
+
+  while(WiFi.status() != WL_CONNECTED){          // Wait until the Wi-Fi connection is established
+    delay(500);                                  // 500ms Wait
+    Serial.print(".");                            // Prints a dot on the console to indicate that the connection is still being established
+  }
+  Serial.println("");
+  Serial.println("WiFi conectado!");             // Prints a message indicating that the Wi-Fi connection was successfully established
+  Serial.print("IP: ");
+  Serial.println(WiFi.localIP());                 // Prints the IP address assigned to the ESP8266 module by the Wi-Fi network
+}
+
+void callback(char* topic, byte* payload, unsigned int length){
+  Serial.print("Mensagem recebida no tópico [");  // Prints a message indicating that a new MQTT message has been received
+  Serial.print(topic);                            // Prints the topic of the received message
+  Serial.print("]: ");
+
+  // Loop through the payload array and print its contents to the serial monitor
+  for(int i = 0; i < length; i++){
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+
+  // Check if the payload's first character is '1'. If it is, turn the built-in LED on, otherwise turn it off.
+  if((int)payload[0] == TURN_ON_COMMAND)
+  {
+    irsend.sendRaw(turnOnSignal.rawData, turnOnSignal.rawDataLength, 38);
+    Serial.println("Ligando o ar-condicionado...");
+    //Serial.println(resultToSourceCode(&turnOnSignal));
+  } 
+  else if ((int)payload[0] == TURN_OFF_COMMAND)
+  {
+    irsend.sendRaw(turnOffSignal.rawData, turnOffSignal.rawDataLength, 38);
+    Serial.println("Desligando o ar-condicionado...");
+    //Serial.println(resultToSourceCode(&turnOffSignal));
+  }
+}
+
+void reconnect(){                                // Function attempts to establish a connection with the MQTT broker.
+  byte willQoS = 0;
+  const char* willTopic = "esp/status";
+  const char* willMessage = "OFF_LINE";
+  boolean willRetain = true;
+
+  // Attempt to connect to the MQTT broker until a connection is established
+  while(!client.connected()){
+    Serial.print("Tentando estabelecer conexão MQTT...");
+
+    // Define the MQTT client ID
+    String clientId = "ESP8266Client01p";
+
+    // Attempt to connect to the MQTT broker with the defined parameters
+    if (client.connect(clientId.c_str(), willTopic, willQoS, willRetain, willMessage)) {
+      Serial.println("Conectado!");
+
+      // Publish a message to the "esp/status" topic to indicate that the device is online
+      char* message = "ON_LINE";
+      int length = strlen(message);
+      boolean retained = true;
+      client.publish("esp/status", (byte*)message, length, retained); // publica status de conexão com WI-FI
+
+      // Subscribe to the "room/12/air-conditioner/1/state" topic
+      client.subscribe("room/12/air-conditioner/1/state");
+      
+    } else {
+      // If a connection cannot be established, print the error message and delay for 5 seconds before trying again
+      Serial.print("Não foi possível se conectar. Estado do esp: ");
+      Serial.print(client.state());
+      Serial.println("Tentando novamente em 5 segundos");
+      delay(5000);
     }
-    else if (readCommand == TURN_OFF_COMMAND)
-    {
-      irsend.sendRaw(turnOffSignal.rawData, turnOffSignal.rawDataLength, 38);
-      Serial.println("Desligando o ar-condicionado...");
-      //Serial.println(resultToSourceCode(&turnOffSignal));
-    }
-    delay(2000);
   }
 }
